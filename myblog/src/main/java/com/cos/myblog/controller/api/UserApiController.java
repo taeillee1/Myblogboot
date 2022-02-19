8af70c109f57bb076779bea9204711cbd704f295
taeillee <leemsdong2@naver.com>
2022-02-19 21:55:40 +0900

    최적화 연습

--- myblog/src/main/java/com/cos/myblog/controller/api/UserApiController.java
+++ myblog/src/main/java/com/cos/myblog/controller/api/UserApiController.java
@@ -4,6 +4,7 @@ import com.cos.myblog.dto.ResponseDto;
 import com.cos.myblog.model.RoleType;
 import com.cos.myblog.model.User;
 import com.cos.myblog.service.UserService;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,8 +26,12 @@ public class UserApiController {
 
 //    @PostMapping("/api/user")
     @PostMapping("/auth/joinProc") //여기서 return 된값이 js에 resp로 들어가는것
-    public ResponseDto<Integer> save(@RequestBody User user){
+    public ResponseDto<Integer> save(@RequestBody CreateMemberRequest request){
         //여기서 DB에 insert를하고 return 하면 js에서 ajax done 부분에 들어간다
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setEmail(request.getEmail());
         userService.save(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //Ok부분에 정상적으로 출력되면 200이 들어간다
     }
@@ -34,6 +39,13 @@ public class UserApiController {
     public ResponseDto<Integer> update(@RequestBody User user){
         userService.userUpdate(user);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String username;
+        private String password;
+        private String email;
     }
 
 //    @PostMapping("/api/user/login")
